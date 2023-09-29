f1aa4f79136888813c7319fc6daecfd649ec88ea
Ronald Galicia <56902637+rgaliciaj@users.noreply.github.com>
2023-09-28 18:51:30 -0600

    consumo de API categorias completado
    Se muestran en la vista y se realizan pruebas de paginador en la tabla

--- angularapp/src/app/componentes/Clientes/consultar-cliente/consultar-cliente.component.ts
+++ angularapp/src/app/componentes/Clientes/consultar-cliente/consultar-cliente.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule, SortDirection } from '@angular/material/sort';
 import { merge, Observable, of as observableOf } from 'rxjs';
@@ -7,90 +7,123 @@ import { catchError, map, startWith, switchMap } from 'rxjs/operators';
 import { MatTableModule } from '@angular/material/table';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { NgIf, DatePipe } from '@angular/common';
+import { ClientesService, categoria, cliente } from 'src/app/servicios/clientes.service';
 
 @Component({
   selector: 'app-consultar-cliente',
   templateUrl: './consultar-cliente.component.html',
   styleUrls: ['./consultar-cliente.component.css']
 })
-export class ConsultarClienteComponent {
-
+export class ConsultarClienteComponent implements OnInit {
   isTableEmpty = true;
 
+  displayedColumns: string[] = ['codigo', 'nombre', 'activo'];
+
+  clientesInfo: cliente[] = [];
+  categoriasInfo: categoria[] = [];
 
 
-
-  displayedColumns: string[] = ['created', 'state', 'number', 'title'];
-  exampleDatabase!: ExampleHttpDatabase | null;
-  data: GithubIssue[] = [];
+  // exampleDatabase!: ExampleHttpDatabase | null;
+  // data: GithubIssue[] = [];
 
   resultsLength = 0;
-  isLoadingResults = true;
+  isLoadingResults = false;
   isRateLimitReached = false;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private _httpClient: HttpClient) { }
+  constructor(private clientservice: ClientesService, private _httpClient: HttpClient) { }
 
-  ngAfterViewInit() {
-    this.exampleDatabase = new ExampleHttpDatabase(this._httpClient);
+  ngOnInit(): void {
+    this.getDataClient();
+    this.getCategorias();
+  }
 
-    // If the user changes the sort order, reset back to the first page.
-    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+  // ngAfterViewInit() {
 
-    merge(this.sort.sortChange, this.paginator.page)
-      .pipe(
-        startWith({}),
-        switchMap(() => {
-          this.isLoadingResults = true;
-          return this.exampleDatabase!.getRepoIssues(
-            this.sort.active,
-            this.sort.direction,
-            this.paginator.pageIndex,
-          ).pipe(catchError(() => observableOf(null)));
-        }),
-        map(data => {
-          // Flip flag to show that loading has finished.
-          this.isLoadingResults = false;
-          this.isRateLimitReached = data === null;
 
-          if (data === null) {
-            return [];
-          }
 
-          // Only refresh the result length if there is new data. In case of rate
-          // limit errors, we do not want to reset the paginator to zero, as that
-          // would prevent users from re-triggering requests.
-          this.resultsLength = data.total_count;
-          return data.items;
-        }),
-      )
-      .subscribe(data => (this.data = data));
+  //   this.exampleDatabase = new ExampleHttpDatabase(this._httpClient);
+
+  //   // If the user changes the sort order, reset back to the first page.
+  //   this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+
+  //   merge(this.sort.sortChange, this.paginator.page)
+  //     .pipe(
+  //       startWith({}),
+  //       switchMap(() => {
+  //         this.isLoadingResults = true;
+  //         return this.exampleDatabase!.getRepoIssues(
+  //           this.sort.active,
+  //           this.sort.direction,
+  //           this.paginator.pageIndex,
+  //         ).pipe(catchError(() => observableOf(null)));
+  //       }),
+  //       map(data => {
+  //         // Flip flag to show that loading has finished.
+  //         this.isLoadingResults = false;
+  //         this.isRateLimitReached = data === null;
+
+  //         if (data === null) {
+  //           return [];
+  //         }
+
+  //         // Only refresh the result length if there is new data. In case of rate
+  //         // limit errors, we do not want to reset the paginator to zero, as that
+  //         // would prevent users from re-triggering requests.
+  //         this.resultsLength = data.total_count;
+  //         return data.items;
+  //       }),
+  //     )
+  //     .subscribe(data => (this.data = data));
+  // }
+
+  getDataClient() {
+    try {
+      this.clientservice.getAllClients().subscribe( (data: any) => {
+        this.clientesInfo = data;
+        console.log('esta es la info: ' + data);
+      })
+    } catch (error) {
+      console.error('Este es el error: ', error)
+    }
+  }
+
+  getCategorias(){
+    try {
+
+      this.clientservice.getAllCat().subscribe( (data: any) => {
+        this.categoriasInfo = data;
+        console.log('las categorias son: ', data);
+      })
+    } catch (error) {
+      console.error('Este es el error: ', error)
+    }
   }
 }
 
-export interface GithubApi {
-  items: GithubIssue[];
-  total_count: number;
-}
+// export interface GithubApi {
+//   items: GithubIssue[];
+//   total_count: number;
+// }
 
-export interface GithubIssue {
-  created_at: string;
-  number: string;
-  state: string;
-  title: string;
-}
+// export interface GithubIssue {
+//   created_at: string;
+//   number: string;
+//   state: string;
+//   title: string;
+// }
 
-/** An example database that the data source uses to retrieve data for the table. */
-export class ExampleHttpDatabase {
-  constructor(private _httpClient: HttpClient) { }
+// /** An example database that the data source uses to retrieve data for the table. */
+// export class ExampleHttpDatabase {
+//   constructor(private _httpClient: HttpClient) { }
 
-  getRepoIssues(sort: string, order: SortDirection, page: number): Observable<GithubApi> {
-    const href = 'https://api.github.com/search/issues';
-    const requestUrl = `${href}?q=repo:angular/components&sort=${sort}&order=${order}&page=${page + 1
-      }`;
+//   getRepoIssues(sort: string, order: SortDirection, page: number): Observable<GithubApi> {
+//     const href = 'https://api.github.com/search/issues';
+//     const requestUrl = `${href}?q=repo:angular/components&sort=${sort}&order=${order}&page=${page + 1
+//       }`;
 
-    return this._httpClient.get<GithubApi>(requestUrl);
-  }
-}
+//     return this._httpClient.get<GithubApi>(requestUrl);
+//   }
+// }
