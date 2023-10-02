935400eb8ca438745c28f2aed2bca8b7ac58c2c8
Ronald Galicia <56902637+rgaliciaj@users.noreply.github.com>
2023-10-02 03:27:15 -0600

    [ADD] se crean apis para obtener listado de datos
    pais, depto, muni, zona y crear un api para el ingreso de creación de cliente

---
+++ ModelsStore/DTO/TABLES/LISTADO_DEPARTAMENTOS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsStore.DTO.TABLES
+{
+    public class LISTADO_DEPARTAMENTOS
+    {
+        public string CODIGO_DEPARTAMENTO {  get; set; }
+        public string DEPARTAMENTO { get; set; }
+        public string DESCRIPCION { get; set; }
+
+    }
+}
---
+++ webapi/Controllers/DepartamentosController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelsStore.DTO.TABLES;
+using ModelsStore.DbConn.DbConect;
+using SqlKata;
+using ClassDB.SqlKataTools;
+
+namespace webapi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DepartamentosController : ControllerBase
+    {
+        [HttpGet("ObtenerDepartamentos")]
+        public IActionResult ObtenerDepartamentos()
+        {
+            ExecuteFromDBMSProvider execute = new ExecuteFromDBMSProvider();
+
+            var connection = new ConectionDecider();
+
+            try
+            {
+                var query = new Query("LISTADO_DEPARTAMENTOS").Select("*");
+
+                var sql = execute.ExecuterCompiler(query);
+
+                var departamentos = new List<LISTADO_DEPARTAMENTOS>();
+
+                execute.DataReader(sql, reader =>
+                {
+                    departamentos = DataReaderMapper<LISTADO_DEPARTAMENTOS>.MapToList(reader);
+                });
+                return Ok(departamentos.ToList());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error en el servidor: {ex.Message}");
+            }
+        }
+    }
+}
